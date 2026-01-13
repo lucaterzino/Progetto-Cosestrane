5fc93540bc334f58f424344ea4977c61e5fd7b8f
lucaterzino <lucaterzino56@gmail.com>
2026-01-13 15:24:19 +0100

    adjusting fight turn, add credits

--- gamelib.c
+++ gamelib.c
@@ -11,15 +11,20 @@ static int undici_preso = 0;
 static int gioco_pronto = 0;
 static int gioco_terminato = 0;
 
+// Albo d'oro per i crediti (ultimi 3 vincitori)
+static char albo_doro[3][100] = {"-", "-", "-"};
+
 // --- PROTOTIPI FUNZIONI INTERNE ---
 static void menu_turno_giocatore(struct Giocatore* g);
 static void avanza(struct Giocatore* g, int* azione_eseguita);
 static void indietreggia(struct Giocatore* g, int* azione_eseguita);
 static void cambia_mondo(struct Giocatore* g, int* azione_eseguita);
-static void combatti(struct Giocatore* g); // Placeholder
+static void combatti(struct Giocatore* g);
+static void stampa_giocatore(struct Giocatore* g);
+static void stampa_zona(struct Giocatore* g);
 static void raccogli_oggetto(struct Giocatore* g);
-static void stampa_info_giocatore(struct Giocatore* g);
-static void stampa_info_zona(struct Giocatore* g);
+static void utilizza_oggetto(struct Giocatore* g);
+static void passa(struct Giocatore* g);
 
 // --- FUNZIONI DI UTILITÀ ---
 
@@ -29,6 +34,13 @@ static int casuale(int min, int max) {
 
 static void pulisci_buffer() {
     while (getchar() != '\n');
+}
+
+static void aggiungi_vincitore(const char* nome) {
+    // Shift dei vincitori (il più vecchio esce)
+    strcpy(albo_doro[2], albo_doro[1]);
+    strcpy(albo_doro[1], albo_doro[0]);
+    strcpy(albo_doro[0], nome);
 }
 
 static void dealloca_mappa() {
@@ -117,6 +129,7 @@ static const char* nome_oggetto(Tipo_oggetto t) {
 }
 
 // --- FUNZIONI CREAZIONE MAPPA ---
+
 static void genera_mappa() {
     if (prima_zona_mondoreale != NULL) dealloca_mappa();
     struct Zona_mondoreale* coda_mr = NULL;
@@ -134,8 +147,7 @@ static void genera_mappa() {
         else if (r_nem <= 90) nuova_mr->nemico = democane;
         else nuova_mr->nemico = billi;
 
-        // Generazione nemici SS - CORREZIONE QUI
-        // Generiamo solo Nessuno o Democane nel loop. Il Demotorzone lo mettiamo dopo.
+        // Generazione nemici SS (Escludiamo Demotorzone qui)
         r_nem = casuale(1, 100);
         if (r_nem <= 60) nuova_ss->nemico = nessun_nemico;
         else nuova_ss->nemico = democane; 
@@ -158,7 +170,6 @@ static void genera_mappa() {
     }
 
     // PIAZZAMENTO UNICO DEMOTORZONE
-    // Scegliamo una zona a caso tra 0 e 14
     int indice_boss = casuale(0, 14);
     struct Zona_soprasotto* cursore = prima_zona_soprasotto;
     for (int k = 0; k < indice_boss; k++) {
@@ -166,7 +177,7 @@ static void genera_mappa() {
     }
     cursore->nemico = demotorzone;
 
-    printf("Mappa generata (15 zone).\n");
+    printf("Mappa generata (15 zone). Il Demotorzone si nasconde nell'oscurita'...\n");
 }
 
 static void inserisci_zona() {
@@ -259,6 +270,85 @@ static void chiudi_mappa() {
     gioco_pronto = 1; printf("Mappa chiusa. Gioco pronto!\n");
 }
 
+// --- HELPER COMBATTIMENTO E GIOCO ---
+
+static void rimuovi_giocatore(struct Giocatore* g) {
+    printf("\n☠️  %s E' MORTO! ☠️\n", g->nome);
+    
+    int giocatori_vivi = 0;
+    for (int i = 0; i < 4; i++) {
+        if (giocatori[i] == g) {
+            free(giocatori[i]);
+            giocatori[i] = NULL;
+        } else if (giocatori[i] != NULL) {
+            giocatori_vivi++;
+        }
+    }
+
+    if (giocatori_vivi == 0) {
+        printf("Tutti i giocatori sono periti nel Sottosopra. GAME OVER.\n");
+        gioco_terminato = 1;
+    }
+}
+
+static int utilizza_oggetto_logic(struct Giocatore* g, int* bonus_attacco, int* bonus_difesa, int* hp_recupero, int in_combattimento) {
+    printf("\n--- ZAINO ---\n");
+    int count_oggetti = 0;
+    for (int i = 0; i < 3; i++) {
+        printf("%d) %s\n", i + 1, nome_oggetto(g->zaino[i]));
+        if (g->zaino[i] != nessun_oggetto) count_oggetti++;
+    }
+    
+    if (count_oggetti == 0) {
+        printf("Lo zaino è vuoto o contiene solo cianfrusaglie inutili.\n");
+        return 0; 
+    }
+
+    printf("Scegli oggetto da usare (0 per annullare): ");
+    int scelta;
+    scanf("%d", &scelta); pulisci_buffer();
+
+    if (scelta < 1 || scelta > 3 || g->zaino[scelta-1] == nessun_oggetto) return 0;
+
+    Tipo_oggetto obj = g->zaino[scelta-1];
+
+    switch (obj) {
+        case maglietta_fuocoinferno:
+            if(in_combattimento) {
+                printf("Indossi la Maglietta Hellfire! (+5 Difesa)\n");
+                *bonus_difesa += 5;
+            } else {
+                printf("Indossi la maglietta. Ti senti molto 'metal', ma non succede nulla di pratico.\n");
+            }
+            break;
+        case schitarrata_metallica:
+            if(in_combattimento) {
+                printf("SUONI UN ASSOLO LEGGENDARIO! (+10 Attacco)\n");
+                *bonus_attacco += 10;
+                g->zaino[scelta-1] = nessun_oggetto; // Si consuma
+                return 1;
+            } else {
+                printf("Suoni un assolo nel nulla. Gli scoiattoli scappano terrorizzati.\n");
+            }
+            break;
+        case bicicletta:
+            if(in_combattimento) {
+                 printf("Usi la bicicletta per schivare e recuperare fiato! (+10 HP)\n");
+                 *hp_recupero += 10;
+                 return 1;
+            } else {
+                 printf("Fai un giro in bici. La tua condizione fisica migliora leggermente. (Solo scenico)\n");
+            }
+            break;
+        case bussola:
+            printf("La bussola gira impazzita... indica che il Demotorzone è da qualche parte nel Soprasotto.\n");
+            break;
+        default:
+            printf("Oggetto non utilizzabile.\n");
+    }
+    return 1; 
+}
+
 // --- FUNZIONI DI GIOCO ---
 
 static void avanza(struct Giocatore* g, int* azione_eseguita) {
@@ -269,11 +359,11 @@ static void avanza(struct Giocatore* g, int* azione_eseguita) {
     else nemico_presente = g->pos_soprasotto->nemico;
 
     if (nemico_presente != nessun_nemico) {
-        printf("Non puoi avanzare! C'è un nemico (%s) che ti blocca. Devi combattere o scappare (cambia mondo)!\n", nome_nemico(nemico_presente));
+        printf("Non puoi avanzare! C'è un nemico (%s) che ti blocca.\n", nome_nemico(nemico_presente));
         return;
     }
 
-    if (g->mondo == 0) { // Mondo Reale
+    if (g->mondo == 0) { 
         if (g->pos_mondoreale->avanti == NULL) {
             printf("Sei all'ultima zona, non puoi avanzare oltre!\n");
         } else {
@@ -282,7 +372,7 @@ static void avanza(struct Giocatore* g, int* azione_eseguita) {
             printf("%s avanza alla zona successiva (%s).\n", g->nome, nome_zona(g->pos_mondoreale->tipo));
             *azione_eseguita = 1;
         }
-    } else { // Soprasotto
+    } else { 
         if (g->pos_soprasotto->avanti == NULL) {
             printf("Sei all'ultima zona, non puoi avanzare oltre!\n");
         } else {
@@ -331,7 +421,6 @@ static void cambia_mondo(struct Giocatore* g, int* azione_eseguita) {
     if (*azione_eseguita) { printf("Hai già mosso in questo turno!\n"); return; }
 
     if (g->mondo == 0) { 
-        // Dalla Realtà al Soprasotto
         if (g->pos_mondoreale->nemico != nessun_nemico) {
             printf("Non puoi cambiare mondo! Devi prima sconfiggere il nemico presente.\n");
             return;
@@ -339,7 +428,6 @@ static void cambia_mondo(struct Giocatore* g, int* azione_eseguita) {
         g->mondo = 1;
         printf("%s viene catapultato nel SOPRASOTTO!\n", g->nome);
     } else {
-        // Dal Soprasotto alla Realtà
         printf("Tentativo di fuga dal Soprasotto... (Tiro Fortuna)\n");
         int tiro = casuale(1, 20);
         printf("Hai tirato: %d (La tua Fortuna: %d)\n", tiro, g->fortuna);
@@ -355,58 +443,133 @@ static void cambia_mondo(struct Giocatore* g, int* azione_eseguita) {
 }
 
 static void combatti(struct Giocatore* g) {
-    printf("[TODO] Fase di combattimento non ancora implementata. (Prossima Fase)\n");
-}
+    Tipo_nemico nemico;
+    void* zona_ptr;
+    int is_mondo_reale = (g->mondo == 0);
 
-static void raccogli_oggetto(struct Giocatore* g) {
-    if (g->mondo == 1) {
-        printf("Non ci sono oggetti nel Soprasotto.\n");
-        return;
-    }
-    if (g->pos_mondoreale->oggetto == nessun_oggetto) {
-        printf("Qui non c'è nulla da raccogliere.\n");
-        return;
-    }
-    if (g->pos_mondoreale->nemico != nessun_nemico) {
-        printf("Non puoi raccogliere oggetti mentre c'è un nemico!\n");
-        return;
+    if (is_mondo_reale) {
+        nemico = g->pos_mondoreale->nemico;
+        zona_ptr = g->pos_mondoreale;
+    } else {
+        nemico = g->pos_soprasotto->nemico;
+        zona_ptr = g->pos_soprasotto;
     }
 
-    // Cerca spazio nello zaino
-    int slot_trovato = -1;
-    for(int i=0; i<3; i++) {
-        if(g->zaino[i] == nessun_oggetto) {
-            slot_trovato = i;
-            break;
+    if (nemico == nessun_nemico) {
+        printf("Non c'è nessun nemico qui da combattere.\n");
+        return;
+    }
+
+    int hp_nemico, attacco_nemico, difesa_nemico;
+    const char* nome_n = nome_nemico(nemico);
+
+    switch (nemico) {
+        case billi: hp_nemico = 20; attacco_nemico = 5; difesa_nemico = 2; break;
+        case democane: hp_nemico = 40; attacco_nemico = 10; difesa_nemico = 5; break;
+        case demotorzone: hp_nemico = 80; attacco_nemico = 15; difesa_nemico = 10; break;
+        default: return;
+    }
+
+    int hp_giocatore = (g->difesa_pischica * 2) + 20;
+    int bonus_attacco = 0, bonus_difesa = 0, hp_recupero = 0;
+
+    printf("\n⚔️  INIZIO COMBATTIMENTO CONTRO %s ⚔️\n", nome_n);
+    printf("HP Nemico: %d | Tuoi HP: %d\n", hp_nemico, hp_giocatore);
+
+    while (hp_giocatore > 0 && hp_nemico > 0) {
+        printf("\n--- SOTTOMENU COMBATTIMENTO ---\n");
+        printf("1) Attacco Pischico\n2) Utilizza Oggetto\nScelta: ");
+        int sc; scanf("%d", &sc); pulisci_buffer();
+        int turno_usato = 0;
+
+        if (sc == 1) {
+            int tiro_fortuna = casuale(0, 20);
+            int is_critico = (tiro_fortuna < g->fortuna);
+            int danno = (g->attacco_pischico + bonus_attacco) - difesa_nemico + casuale(-2, 2);
+            if (danno < 0) danno = 0;
+            if (is_critico) { printf("✨ COLPO CRITICO! ✨\n"); danno *= 2; }
+            hp_nemico -= danno;
+            printf("Hai inflitto %d danni a %s!\n", danno, nome_n);
+            turno_usato = 1;
+        } else if (sc == 2) {
+            hp_recupero = 0;
+            turno_usato = utilizza_oggetto_logic(g, &bonus_attacco, &bonus_difesa, &hp_recupero, 1);
+            hp_giocatore += hp_recupero;
+        }
+
+        if (hp_nemico <= 0) break;
+
+        if (turno_usato) {
+            int danno_subito = attacco_nemico - (g->difesa_pischica + bonus_difesa) + casuale(0, 5);
+            if (danno_subito < 1) danno_subito = 1;
+            hp_giocatore -= danno_subito;
+            printf("%s attacca! Subisci %d danni. (Tuoi HP: %d)\n", nome_n, danno_subito, hp_giocatore);
         }
     }
 
-    if (slot_trovato != -1) {
-        g->zaino[slot_trovato] = g->pos_mondoreale->oggetto;
-        printf("Hai raccolto: %s!\n", nome_oggetto(g->pos_mondoreale->oggetto));
-        g->pos_mondoreale->oggetto = nessun_oggetto; // Rimuovi da terra
+    if (hp_giocatore <= 0) {
+        rimuovi_giocatore(g);
     } else {
-        printf("Zaino pieno! Usa o scarta qualcosa prima.\n");
+        printf("\n🎉 VITTORIA! Hai sconfitto %s! 🎉\n", nome_n);
+        int prob = casuale(1, 100);
+        if (prob <= 50) {
+            printf("Il nemico svanisce...\n");
+            if (is_mondo_reale) ((struct Zona_mondoreale*)zona_ptr)->nemico = nessun_nemico;
+            else ((struct Zona_soprasotto*)zona_ptr)->nemico = nessun_nemico;
+            if (nemico == demotorzone) {
+                printf("\n🏆 HAI SCONFITTO IL BOSS FINALE! VITTORIA! 🏆\n");
+                aggiungi_vincitore(g->nome);
+                gioco_terminato = 1;
+            }
+        } else {
+            printf("Il nemico è a terra ma il corpo rimane lì.\n");
+        }
     }
 }
 
-static void stampa_info_giocatore(struct Giocatore* g) {
+static void stampa_giocatore(struct Giocatore* g) {
     printf("\n--- INFO %s ---\n", g->nome);
     printf("Mondo: %s\n", (g->mondo == 0) ? "Reale" : "Soprasotto");
-    printf("HP/Stato: [Non implementato]\n");
     printf("Zaino: [1]%s [2]%s [3]%s\n", nome_oggetto(g->zaino[0]), nome_oggetto(g->zaino[1]), nome_oggetto(g->zaino[2]));
+    printf("Stats: Atk %d | Def %d | Fortuna %d\n", g->attacco_pischico, g->difesa_pischica, g->fortuna);
 }
 
-static void stampa_info_zona(struct Giocatore* g) {
+static void stampa_zona(struct Giocatore* g) {
     printf("\n--- ZONA ATTUALE ---\n");
     if (g->mondo == 0) {
         printf("Luogo: %s (Reale)\n", nome_zona(g->pos_mondoreale->tipo));
         printf("Nemico: %s\n", nome_nemico(g->pos_mondoreale->nemico));
-        printf("Oggetto a terra: %s\n", nome_oggetto(g->pos_mondoreale->oggetto));
+        printf("Oggetto: %s\n", nome_oggetto(g->pos_mondoreale->oggetto));
     } else {
         printf("Luogo: %s (Soprasotto)\n", nome_zona(g->pos_soprasotto->tipo));
         printf("Nemico: %s\n", nome_nemico(g->pos_soprasotto->nemico));
     }
+}
+
+static void raccogli_oggetto(struct Giocatore* g) {
+    if (g->mondo == 1) { printf("Non ci sono oggetti nel Soprasotto.\n"); return; }
+    if (g->pos_mondoreale->oggetto == nessun_oggetto) { printf("Nessun oggetto qui.\n"); return; }
+    if (g->pos_mondoreale->nemico != nessun_nemico) { printf("Nemico presente! Sconfiggilo prima.\n"); return; }
+
+    int slot = -1;
+    for(int i=0; i<3; i++) if(g->zaino[i] == nessun_oggetto) { slot = i; break; }
+
+    if (slot != -1) {
+        g->zaino[slot] = g->pos_mondoreale->oggetto;
+        printf("Hai raccolto: %s!\n", nome_oggetto(g->pos_mondoreale->oggetto));
+        g->pos_mondoreale->oggetto = nessun_oggetto;
+    } else {
+        printf("Zaino pieno!\n");
+    }
+}
+
+static void utilizza_oggetto(struct Giocatore* g) {
+    int dummy1=0, dummy2=0, dummy3=0;
+    utilizza_oggetto_logic(g, &dummy1, &dummy2, &dummy3, 0);
+}
+
+static void passa(struct Giocatore* g) {
+    printf("%s passa il turno.\n", g->nome);
 }
 
 static void menu_turno_giocatore(struct Giocatore* g) {
@@ -415,16 +578,14 @@ static void menu_turno_giocatore(struct Giocatore* g) {
     int movimento_fatto = 0; 
 
     do {
+        int vivo = 0;
+        for(int i=0; i<4; i++) if(giocatori[i] == g) vivo = 1;
+        if(!vivo) return;
+
         printf("\n=== TURNO DI %s ===\n", g->nome);
-        stampa_info_zona(g);
-        
-        printf("1) Avanza\n");
-        printf("2) Indietreggia\n");
-        printf("3) Cambia Mondo\n");
-        printf("4) Combatti\n");
-        printf("5) Raccogli oggetto\n");
-        printf("6) Info Giocatore\n");
-        printf("7) Passa turno\n");
+        printf("1) Avanza\n2) Indietreggia\n3) Cambia Mondo\n4) Combatti\n");
+        printf("5) Stampa Giocatore\n6) Stampa Zona\n7) Raccogli Oggetto\n");
+        printf("8) Utilizza Oggetto\n9) Passa\n");
         printf("Scelta: ");
         scanf("%d", &scelta); pulisci_buffer();
 
@@ -433,9 +594,11 @@ static void menu_turno_giocatore(struct Giocatore* g) {
             case 2: indietreggia(g, &movimento_fatto); break;
             case 3: cambia_mondo(g, &movimento_fatto); break;
             case 4: combatti(g); break;
-            case 5: raccogli_oggetto(g); break;
-            case 6: stampa_info_giocatore(g); break;
-            case 7: fine_turno = 1; break;
+            case 5: stampa_giocatore(g); break;
+            case 6: stampa_zona(g); break;
+            case 7: raccogli_oggetto(g); break;
+            case 8: utilizza_oggetto(g); break;
+            case 9: passa(g); fine_turno = 1; break;
             default: printf("Comando non valido.\n");
         }
     } while (!fine_turno && !gioco_terminato);
@@ -467,7 +630,6 @@ void imposta_gioco() {
         giocatori[i]->fortuna = casuale(1, 20);
 
         printf("Stats: Atk %d, Def %d, Luck %d\n", giocatori[i]->attacco_pischico, giocatori[i]->difesa_pischica, giocatori[i]->fortuna);
-        
         printf("Modifiche: 0) No, 1) +3/-3, 2) -3/+3");
         if (!undici_preso) printf(", 3) Undici Special");
         printf("\nScelta: "); int sc; scanf("%d", &sc); pulisci_buffer();
@@ -500,11 +662,12 @@ void gioca() {
     if (!gioco_pronto) { printf("Errore: Gioco non impostato.\n"); return; }
     gioco_terminato = 0;
 
-    // 1. Posiziona tutti i giocatori all'inizio
     for(int i=0; i<numero_giocatori; i++) {
-        giocatori[i]->pos_mondoreale = prima_zona_mondoreale;
-        giocatori[i]->pos_soprasotto = prima_zona_soprasotto;
-        giocatori[i]->mondo = 0; 
+        if(giocatori[i] != NULL) {
+            giocatori[i]->pos_mondoreale = prima_zona_mondoreale;
+            giocatori[i]->pos_soprasotto = prima_zona_soprasotto;
+            giocatori[i]->mondo = 0; 
+        }
     }
 
     printf("\n--- INIZIO PARTITA ---\n");
@@ -524,10 +687,15 @@ void gioca() {
         for (int i = 0; i < numero_giocatori; i++) {
             int idx = ordine[i];
             if (giocatori[idx] == NULL) continue;
-            
             menu_turno_giocatore(giocatori[idx]);
-            
             if (gioco_terminato) break;
+        }
+        
+        int vivi = 0;
+        for(int k=0; k<numero_giocatori; k++) if(giocatori[k] != NULL) vivi++;
+        if(vivi == 0 && numero_giocatori > 0) {
+            printf("Tutti morti. Game Over.\n");
+            gioco_terminato = 1;
         }
     }
 }
@@ -538,5 +706,8 @@ void termina_gioco() {
 }
 
 void crediti() {
-    printf("Creato da: Tuo Nome\n");
+    printf("\n--- CREDITI ---\n");
+    printf("Sviluppato da: Luca Terzino\n");
+    printf("\n--- ALBO D'ORO (Ultimi 3 Vincitori) ---\n");
+    for(int i=0; i<3; i++) printf("%d. %s\n", i+1, albo_doro[i]);
 }
